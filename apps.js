a504add50acd741c61d8dfa108900eff0c36d4ae
Danilo Novaes <danilo.novaes@live.com>
2018-04-16 19:55:01 -0300

    version02

--- apps.js
+++ apps.js
@@ -2,7 +2,13 @@ var http = require('http');
 
 var server = http.createServer(function (req, res) {
 
-    res.end('<html><body><h1>ALOOOO</h1></body></html>');
+    if (req.url === "/editar") {
+        res.end('<html><body><h1>ALOOOO 2</h1></body></html>');
+    } else {
+        res.end('<html><body><h1>ALOOOO</h1></body></html>');
+    }
+
+
 
 });
 
